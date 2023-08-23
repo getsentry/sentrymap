3af1995f02626b08acbfe70fd5398b6d6123d315
Anton Pirker <anton.pirker@sentry.io>
2023-08-23 14:57:28 +0200

    Hacking

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ import json
 
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
 
 from github_organization import load_data
 
@@ -9,6 +10,20 @@ import redis
 
 
 app = FastAPI(debug=True)
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:9000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 templates = Jinja2Templates(directory="templates")
 
@@ -32,7 +47,13 @@ async def home(request: Request):
             }
         )
 
-    return countries
+    from maps import HexGrid
+    grid = HexGrid(21, 29) 
+    # grid.grow_chunk2(sum(c["size"] for c in countries))
+    grid.grow_chunk2(22)
 
+    return {
+        "grid": grid.grid,
+    }
 
 load_data()
---
+++ frontend/assets/app.js
@@ -0,0 +1,42 @@
+const tileWidth = 64;
+const tileHeight = 96;
+
+const tileIdToBiom = {
+    0: 1,
+    1: 1,
+}
+
+const tileIdToTileset = {
+    0: "tileset_sand",
+    1: "tileset_tropical",
+}
+
+
+function putImage(mapId, stuffId, x, y){
+    var ctx = document.getElementById(mapId).getContext('2d');
+    ctx.drawImage(window.stuff[stuffId], x, y);            
+}
+
+
+function drawTile(mapId, tileSet, tileId, x, y){
+    var ctx = document.getElementById(mapId).getContext('2d');
+    ctx.drawImage(window.stuff[tileSet], (tileId-1)*tileWidth, 0, tileWidth, tileHeight, x, y, tileWidth, tileHeight);
+}
+
+
+function drawTileMap(data) {
+    marginX = tileWidth/4;
+
+    for (let row = 0; row < data.length; row++) {
+        var oneRow = data[row];
+        let offsetX = 0;
+        if(row%2 != 0){
+            offsetX = tileWidth/2;
+        }
+        for (let col = 0; col < oneRow.length; col++) {
+            let offsetY = -row/2*tileHeight;
+            let tileId = oneRow[col];
+            drawTile("map", tileIdToTileset[tileId], tileIdToBiom[tileId], marginX + offsetX + (col)*tileWidth, offsetY + (row)*tileHeight);
+        }
+    }
+}
